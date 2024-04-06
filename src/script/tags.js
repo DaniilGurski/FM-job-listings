78d0d89acd4baa3ef29e95b3e0ea92fe39563c7d
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2024-04-06 11:40:06 +0200

    Add clear filters button and remove unused code

--- src/script/tags.js
+++
@@ -1,5 +0,0 @@
-// TODO: add event listener to a tag button
-// TODO: if clicked, move to filter bar 
-    // TODO: pass text of tag button 
-    // TODO: create a new list item with the passed text
-    // TODO: add close button with event listener to remove the tag from the bar
